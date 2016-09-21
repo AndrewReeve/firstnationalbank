86ba9bdf5cb85070e64b256d81c7ff6e37d45b5d
Andrew Reeve <andrew.126@hotmail.com>
2016-09-21 13:46:25 +1200

    A2 Updates, Beginning the includes and an array of users to get started

--- a2.php
+++ a2.php
@@ -1,9 +1,21 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Andrew
+ * Internet Programming - Assignment 2
+ * Author: Andrew Reeve, 14075224
+ * Author: Jasper Ballantyne, 13115419
+ * Author: Ian Chan, 13103607
  * Date: 21/09/2016
- * Time: 12:37 PM
+ *
+ * a2.php controls the main flow of the first national bank website. This includes Users logging in and registering, and passing off
+ * flow of control to supporitng files such as Account.php
  */
+
+// include "user.php";
+// include "account.php";
+// include "transaction.php";
+
+$users = array(); // An Array of User Objects
+
+
 
 
